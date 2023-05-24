f24b939fffe8dd306106fb8b599a47f791d24d3b
Alexander Giraldo <alexander.giraldo08@gmail.com>
2023-05-24 16:20:40 +0000

    class 7

--- curso-react-intro-main/src/TodoSearch.js
+++ curso-react-intro-main/src/TodoSearch.js
@@ -1,8 +1,17 @@
+import React, { useState } from 'react' ;
 import './TodoSearch.css';
 
 function TodoSearch(){
+    const [searchValue, setSearchValue] = React.useState();
+    useState('');
+    
     return(
-        <input className="TodoSearch" placeholder="Cortar cebolla" />
+        <input className="TodoSearch" placeholder="Cortar cebolla"
+        value={searchValue}
+        onChange={ (event) => {
+            setSearchValue(event.target.value);
+        }}
+        />
     );
 }
 
